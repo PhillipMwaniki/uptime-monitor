8947bd4c812ffc6b710c9f527097b5d252f4bac2
Phillip Mwaniki <codezeroprivate@gmail.com>
2023-02-24 17:08:31 +0300

    formatted emails

--- resources/views/emails/endpoint_recovered.blade.php
+++ resources/views/emails/endpoint_recovered.blade.php
@@ -1,2 +1,12 @@
-Howdy,
-Endpoint {{ $endpoint->id }} is back up.
+<x-mail::message>
+    # {{ $endpoint->location }} recovered
+
+    Great news! {{ $endpoint->url() }} has recovered.
+
+    <x-mail::button :url="route('dashboard', $endpoint->site)">
+        Visit dashboard
+    </x-mail::button>
+
+    Thanks,<br>
+    {{ config('app.name') }}
+</x-mail::message>
